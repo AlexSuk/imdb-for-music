c3905c92b8ee1b1ccc1100115704fbb84f2a5c97
joseramirez270 <jose.ramirez270@gmail.com>
2017-02-13 19:13:07 -0500

    Built tables for Walking Skeleton database using scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :release_groups
+  resources :artists
   root "search#index"
   get '/artist', to: 'search#artist'
   get '/search/:query' => 'search#search', :as => 'search'
