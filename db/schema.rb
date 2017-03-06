c9cd07ef7d4bb603f3c3e5aa29359ba13ddace3b
joseramirez270 <jose.ramirez270@gmail.com>
2017-03-05 20:13:20 -0500

    Added releasegroup_id field to Release

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170305011847) do
+ActiveRecord::Schema.define(version: 20170306010252) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -38,10 +38,11 @@ ActiveRecord::Schema.define(version: 20170305011847) do
     t.string   "name"
     t.date     "release_date"
     t.string   "country"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.integer  "artist_id"
     t.string   "mbid"
+    t.integer  "releasegroup_id"
   end
 
   create_table "tracks", force: :cascade do |t|
