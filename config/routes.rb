0c6567c401e2269273b0c20b4762f1ece9741353
joseramirez270 <jose.ramirez270@gmail.com>
2017-03-05 20:41:17 -0500

    Added User and Review through scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :reviews
+  resources :users
   resources :release_groups
   #resources :artists
+  #resources :releases
   root "search#index"
   get '/artist', to: 'search#artist'
   get '/search/:query' => 'search#search', :as => 'search'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306010252) do
+ActiveRecord::Schema.define(version: 20170306012905) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -45,11 +45,24 @@ ActiveRecord::Schema.define(version: 20170306010252) do
     t.integer  "releasegroup_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string   "review"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tracks", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "release_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
