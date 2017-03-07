c97baf35ff284ca09da5bed86cf75d8f498763f7
joseramirez270 <jose.ramirez270@gmail.com>
2017-03-07 14:48:52 -0500

    Added name validations and added passwords for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 50}
+
+
+  # has_secure_password can:
+  # 1.  save securely hashed password_digest attribute to database
+  # 2.  creates virtual attributes :password and :password_confirmation
+  #     for those attributes, validation for presence and match is
+  #     included
+  # 3.  create authenticate() method which returns a user when pw is correct
+  # model requires password_digest attribute
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
   has_many :reviews
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,48 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    # Test author: Jose Ramirez
+    @user = User.new(name: "Example", password: "foobar",
+                     password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    # Test author: Jose Ramirez
+    assert @user.valid?
+  end
+
+  #validate name presence
+  test "name should be present" do
+    # Test author: Jose Ramirez
+    @user.name = "      "
+    assert_not @user.valid?
+  end
+
+  # validate name length
+  test "name is no more than 50 characters long" do
+    # Test author: Jose Ramirez
+    @user.name = "A" * 51
+    assert_not @user.valid?
+  end
+
+  # email address presence?
+  # validate email address format?
+  # email address uniqueness?
+  # saving email addresses as lowercase?
+
+  # password present?
+  test "password should be nonblank" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  # password minimum length?
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+
 end
