a1746ed899abac3a2c6737337a8318baf266ce94
joseramirez270 <jose.ramirez270@gmail.com>
2017-03-08 13:24:07 -0500

    added email tests to user_test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,9 +31,51 @@ class UserTest < ActiveSupport::TestCase
   end
 
   # email address presence?
+  test "email should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  # validate email length
+  test "email should not be to long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
   # validate email address format?
+  test "email validation should accept valid addresses" do
+    valid_addresses = valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  # email validation should reject invalid addresses
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
   # email address uniqueness?
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
   # saving email addresses as lowercase?
+  test "email addresses shoudl be saved as lowercase" do
+    mixed_case_email = "Foo@ExAMple.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 
   # password present?
   test "password should be nonblank" do
@@ -46,6 +88,4 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-
-
 end
