46a47170c48b7f753c060b853c4791698f76fc9b
Anirudh Mahajan <amahajan@brandeis.edu>
2017-03-29 15:23:03 -0400

    add log out, session destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,11 +18,22 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
+
+  def destroy
+      log_out
+      redirect_to new_session_path
+    end
 end
+
 
 def log_in(user)
   session[:user_id] = user.id
 end
+
+def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 
 private
 
