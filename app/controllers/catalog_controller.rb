6db68e0f85e734908631625b0cf75223bd912d3d
Ryan Johnson <rjohnson465@gmail.com>
2017-03-30 20:58:17 -0400

    set up artist page for band members

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -4,11 +4,32 @@ class CatalogController < ApplicationController
 
   def artist
     artist = Musicbrainz_db.find("artist",params["format"])
-    byebug
     @name = artist["name"]
     @country = artist["country"]
     @id = artist["id"] # note this is the MusicBrainz-assigned id, not the rails assigned one
     @release_groups = artist["release-groups"].sort_by { |hash| hash['first-release-date']}
+
+    relations = artist["relations"]
+    @artist_relations = []
+    @url_relations = []
+    relations.each do |relation|
+      if relation["target-type"] == "url"
+        @url_relations << relation
+      elsif relation["target-type"] == "artist"
+        @artist_relations << relation
+      end
+    end
+
+    # get band members, if there are any
+    @band_members = []
+    @artist_relations.each do |a_relation|
+      if a_relation["type"] == "member of band"
+        @band_members << a_relation
+      end
+    end
+
+    byebug
+
   end
 
   def release_group
