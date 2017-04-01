21b96116cc053b2ca419f0c3c2357b7132994f45
Ryan Johnson <rjohnson465@gmail.com>
2017-04-01 18:45:22 -0400

    Better images for artist. Still get Too Many HTTPRequests a lot, not sure what to do about it

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -70,32 +70,44 @@ class CatalogController < ApplicationController
   def parse_relations_into_images url_relations
     imgurls = []
     url_relations.each do |relation|
+      puts "url relation"
       url = relation["url"]["resource"]
-      doc = Nokogiri::HTML(open(url))
       case relation["type"]
       when "allmusic"
+        doc = Nokogiri::HTML(open(url))
         # parse for allmusic.com
         imgurl = doc.css(".artist-image").children.css("img").attribute("src").value
         imgurls << imgurl
         # TODO -- can we get all images in lightbox gallery?
       when "bandsintown"
+        doc = Nokogiri::HTML(open(url))
         imgurl = doc.css(".sidebar-image").css("img").attribute("src").value
         imgurls << imgurl
       when "discogs"
+        doc = Nokogiri::HTML(open(url))
         # parse for discogs.com
-
-        gallery_url = "https://www.discogs.com" +
+        gallery_url = ""
+        if doc.css(".image_gallery_more").count != 0
+          gallery_url = "https://www.discogs.com" +
                       doc.css(".image_gallery_more").children.css("a").attribute("href").value
-        doc = Nokogiri::HTML(open(gallery_url))
-        gallery = doc.css('[id="view_images"]').children.css("p")
-        i = 0
-        gallery.each do |element|
-          puts "in discogs" + i.to_s
-          if (element.css("img") && element.css("img") != [])
-            imgurl = element.css("img").attribute("src").value
-            imgurls << imgurl
+        elsif doc.css(".image_gallery").count != 0
+          gallery_url = "https://www.discogs.com" +
+                      doc.css(".image_gallery").css("a").attribute("href").value
+        end
+
+        if gallery_url != ""
+          doc = Nokogiri::HTML(open(gallery_url))
+          gallery = doc.css('[id="view_images"]').children.css("p")
+          i = 0
+          gallery.each do |element|
+            if (element.children.css("img").count != 0)
+              puts "in discogs" + i.to_s
+              imgurl = element.css("img").attribute("src").value
+              imgurls << imgurl
+              puts imgurls
+            end
+            i = i + 1
           end
-          i = i + 1
         end
 
       when "last.fm"
@@ -108,12 +120,10 @@ class CatalogController < ApplicationController
     end
 
     return imgurls
-
-
-    # TODO get artist social media, official site, etc
-
     # check for duplicate images, maybe use phasion https://github.com/westonplatter/phashion
-
   end
+
+  # TODO get artist social media, official site, etc
+
 
 end
