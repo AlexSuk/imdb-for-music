25d6d6bfd3c849fee5268c3dd41d15c16cc95da0
joseramirez270 <jose.ramirez270@gmail.com>
2017-04-03 16:47:27 -0400

    Add views for comments, modify post views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   resources :users
-  resources :posts do
-    member do
-      get 'reply'
-    end
-  end
 
   root "static_pages#index"
   get '/artist', to: 'static_pages#artist'
   get '/search/:query' => 'static_pages#search', :as => 'search'
   get '/signup', to: 'users#new'
+
+  resources :posts do
+    resources :comments
+  end
+
+  resources :comments do
+    resources :comments
+  end
 end
