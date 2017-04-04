c5591441679eb3e0bd734703b85e00f930b24bf5
Ryan Johnson <rjohnson465@gmail.com>
2017-04-04 15:17:35 -0400

    Title correction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
     # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "IMDB For Music"
+    base_title = "MeloDB"
     if page_title.empty?
       base_title
     else
