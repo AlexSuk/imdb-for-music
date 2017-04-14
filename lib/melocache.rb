91b7a6d1e7fdfe092a79ad7e420dc86f14581c7f
samuelakerman <samsk66@gmail.com>
2017-04-14 13:02:19 -0400

    redis notification

--- lib/melocache.rb
+++ lib/melocache.rb
@@ -1,15 +1,18 @@
 module MeloCache
 
   def MeloCache.get key
+    puts "redisGet"
     $redis.get key
   end
 
   def MeloCache.set key, value
+    puts "redisSet"
     $redis.set key, value
   end
 
 
   def MeloCache.exists key
+    puts "redisExists"
     $redis.exists key
   end
 
