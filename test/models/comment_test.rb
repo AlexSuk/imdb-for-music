1b5c5fab8efaa219455c193d4fef0c7af874dc1d
joseramirez270 <jose.ramirez270@gmail.com>
2017-04-26 15:17:27 -0400

    Add seed data  comments, allow user id to be passed to posts/comments automatically

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:archer)
+    @post = @user.posts.last
+    @comment = @post.comments.build(body: "Etc etc", user_id: @user.id )
+  end
+
+  # valid
+  test "should be valid" do
+    assert @comment.valid?
+  end
+
+  # user id present
+  test "user id should be present" do
+    @comment.user_id = nil
+    assert_not @comment.valid?
+  end
+
+  # body present
+  test "body should be present" do
+    @comment.body = "     "
+    assert_not @comment.valid?
+  end
+
+  # comments cannot be made without a post?
 end
