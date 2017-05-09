7e112407ea648a9c125bf8c328dde56b72bcf558
Ryan Johnson <rjohnson465@gmail.com>
2017-05-09 10:03:33 -0400

    Fix ratings

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -13,23 +13,23 @@ class CatalogController < ApplicationController
   def release_group
     @release_group = ReleaseGroup.new(params["format"])
     @posts = Post.where("mbid = ?", @release_group.artist["id"]).paginate(page: params[:page])
-    @ratings_hash = get_rating_data(@release_group)
+    @ratings_hash = get_rating_data(@release_group, "release-group")
     @reviews = Review.where("reviews.link = ?", "/release-group.#{@release_group.id}")
   end
 
   def recording
     @recording = Recording.new(params["format"])
     @posts = Post.where("mbid = ?", @recording.artist["id"]).paginate(page: params[:page])
-    @ratings_hash = get_rating_data(@recording)
+    @ratings_hash = get_rating_data(@recording, "recording")
     @reviews = Review.where("reviews.link = ?", "/recording.#{@recording.id}")
   end
 
   private
 
-  def get_rating_data entity
+  def get_rating_data entity, type
     hash = {:avg=> nil, :user=> nil}
-    avg = Review.where("reviews.link = ?", "/recording.#{entity.id}").average(:rating)
-    user = Review.where("reviews.link = ? AND reviews.user_id = ?", "/recording.#{entity.id}", session[:user_id]).pluck(:rating).first
+    avg = Review.where("reviews.link = ?", "/#{type}.#{entity.id}").average(:rating)
+    user = Review.where("reviews.link = ? AND reviews.user_id = ?", "/#{type}.#{entity.id}", session[:user_id]).pluck(:rating).first
     hash[:avg] = avg == nil ? "Not Rated" : avg.round(1)
     hash[:user] = user == nil ? "Not Rated" : user
     return hash
