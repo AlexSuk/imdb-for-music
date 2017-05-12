74b6a154d0d818b4841555f10820569eef6edb62
joseramirez270 <jose.ramirez270@gmail.com>
2017-05-11 20:50:23 -0400

    Remove non-existent type attribute in Favorites fixture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506012048) do
+ActiveRecord::Schema.define(version: 20170509040420) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170506012048) do
     t.integer  "user_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "picture"
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -38,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170506012048) do
     t.datetime "updated_at", null: false
     t.string   "mbid"
     t.string   "url"
+    t.string   "picture"
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
   end
 
